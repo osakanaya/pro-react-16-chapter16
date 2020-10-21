14db5b20e3e5e15cde8fe45fdeddda1d05771747
sashida <sashida@primagest.co.jp>
2020-10-21 09:06:33 +0900

    p.466

--- src/Editor.js
+++ src/Editor.js
@@ -3,43 +3,42 @@ import React, { Component } from "react";
 export class Editor extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            name: "",
-            category: "",
-            price: ""
-        };
         this.nameRef = React.createRef();
-    }
-
-    handleChange = (event) => {
-        event.persist();
-        this.setState(state => state[event.target.name] = event.target.value);
+        this.categoryRef = React.createRef();
+        this.priceRef = React.createRef();
     }
 
     handleAdd = () => {
-        this.props.callback(this.state);
-        this.setState({ name: "", category: "", price: "" },
-            () => this.nameRef.current.focus());
+        this.props.callback({
+            name: this.nameRef.current.value,
+            category: this.categoryRef.current.value,
+            price: this.priceRef.current.value
+        });
+
+        this.nameRef.current.value = "";
+        this.categoryRef.current.value = "";
+        this.priceRef.current.value = "";
+        this.nameRef.current.focus();
     }
 
     render() {
         return <React.Fragment>
             <div className="form-group p-2">
                 <label>Name</label>
-                <input className="form-control" name="name" value={ this.state.name }
-                    onChange={ this.handleChange } autoFocus={ true } ref={ this.nameRef } />
+                <input className="form-control" name="name" 
+                    autoFocus={ true } ref={ this.nameRef } />
             </div>
 
             <div className="form-group p-2">
                 <label>Category</label>
-                <input className="form-control" name="category" value={ this.state.category }
-                    onChange={ this.handleChange } />
+                <input className="form-control" name="category" 
+                    ref={ this.categoryRef } />
             </div>
 
             <div className="form-group p-2">
                 <label>Price</label>
-                <input className="form-control" name="price" value={ this.state.price }
-                    onChange={ this.handleChange } />
+                <input className="form-control" name="price" 
+                    ref={ this.priceRef } />
             </div>
 
             <div className="text-center">
