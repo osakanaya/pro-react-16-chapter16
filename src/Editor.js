a0dc00c46ea21419bd49eb7aa8321096c5016248
sashida <sashida@primagest.co.jp>
2020-10-21 09:09:25 +0900

    p.468

--- src/Editor.js
+++ src/Editor.js
@@ -3,22 +3,28 @@ import React, { Component } from "react";
 export class Editor extends Component {
     constructor(props) {
         super(props);
-        this.nameRef = React.createRef();
-        this.categoryRef = React.createRef();
-        this.priceRef = React.createRef();
+        this.formElements = {
+            name: {},
+            category: {},
+            price: {}
+        };
+    }
+
+    setElement = (element) => {
+        if (element !== null) {
+            this.formElements[element.name].element = element;
+        }
     }
 
     handleAdd = () => {
-        this.props.callback({
-            name: this.nameRef.current.value,
-            category: this.categoryRef.current.value,
-            price: this.priceRef.current.value
+        let data = {};
+        Object.values(this.formElements).forEach(v => {
+            data[v.element.name] = v.element.value;
+            v.element.value = "";
         });
-
-        this.nameRef.current.value = "";
-        this.categoryRef.current.value = "";
-        this.priceRef.current.value = "";
-        this.nameRef.current.focus();
+        
+        this.props.callback(data);
+        this.formElements.name.element.focus();
     }
 
     render() {
@@ -26,19 +32,19 @@ export class Editor extends Component {
             <div className="form-group p-2">
                 <label>Name</label>
                 <input className="form-control" name="name" 
-                    autoFocus={ true } ref={ this.nameRef } />
+                    autoFocus={ true } ref={ this.setElement } />
             </div>
 
             <div className="form-group p-2">
                 <label>Category</label>
                 <input className="form-control" name="category" 
-                    ref={ this.categoryRef } />
+                    ref={ this.setElement } />
             </div>
 
             <div className="form-group p-2">
                 <label>Price</label>
                 <input className="form-control" name="price" 
-                    ref={ this.priceRef } />
+                    ref={ this.setElement } />
             </div>
 
             <div className="text-center">
